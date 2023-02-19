d792b5455e5ae8ff93273aafa3c5ace0d0c92d82
suya172 <asahihysk@gmail.com>
2023-02-19 23:02:08 +0900

    c++17に移行

--- abc/hoge.cpp
+++ abc/hoge.cpp
@@ -1,313 +1,87 @@
-#include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-#include<ext/pb_ds/tag_and_trait.hpp>
-#define overload4(_1, _2, _3, _4, name, ...) name
-#define rep1(i, n) for (ll i = 0; i < ll(n); ++i)
-#define rep2(i, s, n) for (ll i = ll(s); i < ll(n); ++i)
-#define rep3(i, s, n, d) for(ll i = ll(s); i < ll(n); i+=d)
-#define rep(...) overload4(__VA_ARGS__,rep3,rep2,rep1)(__VA_ARGS__)
-#define rrep1(i, n) for (ll i = ll(n)-1; i >= 0; i--)
-#define rrep2(i, n, t) for (ll i = ll(n)-1; i >= (ll)t; i--)
-#define rrep3(i, n, t, d) for (ll i = ll(n)-1; i >= (ll)t; i-=d)
-#define rrep(...) overload4(__VA_ARGS__,rrep3,rrep2,rrep1)(__VA_ARGS__)
-#define all(a) a.begin(),a.end()
-#define rall(a) a.rbegin(),a.rend()
-#define SUM(a) accumulate(all(a),0LL)
-#define MIN(a) *min_element(all(a))
-#define MAX(a) *max_element(all(a))
-#define SORT(a) sort(all(a));
-#define REV(a) reverse(all(a));
-#define SZ(a) int(a.size())
-#define popcount(x) __builtin_popcountll(x)
-#define pf push_front
-#define pb push_back
-#define ef emplace_front
-#define eb emplace_back
-#define ppf pop_front
-#define ppb pop_back
-#ifdef __LOCAL
-#define debug(...) { cout << #__VA_ARGS__; cout << ": "; print(__VA_ARGS__); cout << flush; }
-#else
-#define debug(...) void(0);
-#endif
-#define INT(...) int __VA_ARGS__;scan(__VA_ARGS__)
-#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
-#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
-#define CHR(...) char __VA_ARGS__;scan(__VA_ARGS__)
-#define DBL(...) double __VA_ARGS__;scan(__VA_ARGS__)
-#define LD(...) ld __VA_ARGS__;scan(__VA_ARGS__)
+#include <bits/stdc++.h>
 using namespace std;
-using namespace __gnu_pbds;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
+
+using ull = unsigned long long;
 using ll = long long;
 using ld = long double;
-using P = pair<int, int>;
-using LP = pair<ll, ll>;
+const ll MOD=1000000007;
+const ld EPS=1e-14;
+const int INF=1e+9;
+const ll LINF=1e+18;
+// pair
+using pii = pair<int, int>;
+// vector
 using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
 using vvi = vector<vi>;
-using vvvi = vector<vvi>;
-using vl = vector<ll>;
 using vvl = vector<vl>;
-using vvvl = vector<vvl>;
-using vd = vector<double>;
-using vvd = vector<vd>;
+using vvll = vector<vll>;
 using vs = vector<string>;
-using vc = vector<char>;
-using vvc = vector<vc>;
+using vpii = vector<pii>;
 using vb = vector<bool>;
 using vvb = vector<vb>;
-using vp = vector<P>;
-using vvp = vector<vp>;
-using vlp = vector<LP>;
-using vvlp = vector<vlp>;
-template<class T>
-using PQ = priority_queue<T>;
-template<class T>
-using PQrev = priority_queue<T, vector<T>, greater<T>>;
+// unordered set
+using usi = unordered_set<int>;
+using usll = unordered_set<ll>;
+using uss = unordered_set<string>;
 
-template<class S, class T>
-istream &operator>>(istream &is, pair<S, T> &p) { return is >> p.first >> p.second; }
+#define pb push_back
+#define mp make_pair
+#define um unordered_map
+#define siz size()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+#define itn int
+#define stirng string
 
-template<class S, class T>
-ostream &operator<<(ostream &os, const pair<S, T> &p) { return os << '{' << p.first << ", " << p.second << '}'; }
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
 
-template<class S, class T, class U>
-istream &operator>>(istream &is, tuple<S, T, U> &t) { return is >> get<0>(t) >> get<1>(t) >> get<2>(t); }
+/* debug */
+// 標準エラー出力を含む提出はrejectされる場合もあるので注意
+#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
 
-template<class S, class T, class U>
-ostream &operator<<(ostream &os, const tuple<S, T, U> &t) {
-    return os << '{' << get<0>(t) << ", " << get<1>(t) << ", " << get<2>(t) << '}';
+/* func */
+inline int in_int() {int x; cin >> x; return x;}
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline double in_double() {{double x; cin >> x; return x;}}
+inline string in_str() {string x; cin >> x; return x;}
+inline int ctoi(char c) {return c - '0';}
+// vector_finder: (arg)elementを vectorの先頭から(arg)search_lengthまで先頭から検索し、boolを返す
+// (arg)search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
+template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
+    auto itr = std::find(vec.begin(), vec.end(), element);
+    size_t index = std::distance( vec.begin(), itr );
+    if (index == vec.size() || index >= search_length) {return false;} else {return true;}
 }
+template <typename T> inline void print(const vector<T>& v, string s = " ")
+    {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p)
+    {cout << p.first << " " << p.second << endl;}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
+    {for (auto&& p : v) print(p);}
+template <typename T, typename S> inline void print(const map<T, S>& m)
+    {for (auto&& p : m) print(p);}
+inline void dprint(const double& d,ll n = 10){cout << fixed << setprecision(n) << d << endl;}
+// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
 
-template<class T>
-istream &operator>>(istream &is, vector<T> &v) {
-    for (T &t: v) { is >> t; }
-    return is;
-}
-
-template<class T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
-    os << '[';
-    rep(i, v.size()) os << v[i] << (i == int(v.size() - 1) ? "" : ", ");
-    return os << ']';
-}
-
-template<class T>
-ostream &operator<<(ostream &os, const deque<T> &v) {
-    os << '[';
-    rep(i, v.size()) os << v[i] << (i == int(v.size() - 1) ? "" : ", ");
-    return os << ']';
-}
-
-template<class T>
-ostream &operator<<(ostream &os, const set<T> &st) {
-    os << '{';
-    auto it = st.begin();
-    while (it != st.end()) {
-        os << (it == st.begin() ? "" : ", ") << *it;
-        it++;
-    }
-    return os << '}';
-}
-
-template<class T>
-ostream &operator<<(ostream &os, const multiset<T> &st) {
-    os << '{';
-    auto it = st.begin();
-    while (it != st.end()) {
-        os << (it == st.begin() ? "" : ", ") << *it;
-        it++;
-    }
-    return os << '}';
-}
-
-template<class T, class U>
-ostream &operator<<(ostream &os, const map<T, U> &mp) {
-    os << '{';
-    auto it = mp.begin();
-    while (it != mp.end()) {
-        os << (it == mp.begin() ? "" : ", ") << *it;
-        it++;
-    }
-    return os << '}';
-}
-
-template<class T>
-void vecout(const vector<T> &v, char div = '\n') {
-    rep(i, v.size()) cout << v[i] << (i == int(v.size() - 1) ? '\n' : div);
-}
-
-template<class T>
-bool constexpr chmin(T &a, T b) {
-    if (a > b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-
-template<class T>
-bool constexpr chmax(T &a, T b) {
-    if (a < b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-
-void scan() {}
-
-template<class Head, class... Tail>
-void scan(Head &head, Tail &... tail) {
-    cin >> head;
-    scan(tail...);
-}
-
-template<class T>
-void print(const T &t) { cout << t << '\n'; }
-
-template<class Head, class... Tail>
-void print(const Head &head, const Tail &... tail) {
-    cout << head << ' ';
-    print(tail...);
-}
-
-template<class T>
-vector<T> &operator+=(vector<T> &v, T x) {
-    for (T &t: v) t += x;
-    return v;
-}
-
-template<class T>
-vector<T> &operator-=(vector<T> &v, T x) {
-    for (T &t: v) t -= x;
-    return v;
-}
-
-template<class T>
-vector<T> &operator*=(vector<T> &v, T x) {
-    for (T &t: v) t *= x;
-    return v;
-}
-
-template<class T>
-vector<T> &operator/=(vector<T> &v, T x) {
-    for (T &t: v) t /= x;
-    return v;
-}
-
-struct Init_io {
-    Init_io() {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
-        cout << boolalpha << fixed << setprecision(15);
-        cerr << boolalpha << fixed << setprecision(15);
-    }
-} init_io;
-
-const string yes[] = {"no", "yes"};
-const string Yes[] = {"No", "Yes"};
-const string YES[] = {"NO", "YES"};
-const int inf = 1001001001;
-const ll linf = 1001001001001001001;
-
-void rearrange(const vi &) {}
-
-template<class T, class... Tail>
-void rearrange(const vi &ord, vector<T> &head, Tail &...tail) {
-    assert(ord.size() == head.size());
-    vector<T> ori = head;
-    rep(i, ord.size()) head[i] = ori[ord[i]];
-    rearrange(ord, tail...);
-}
-
-template<class T, class... Tail>
-void sort_by(vector<T> &head, Tail &... tail) {
-    vi ord(head.size());
-    iota(all(ord), 0);
-    sort(all(ord), [&](int i, int j) { return head[i] < head[j]; });
-    rearrange(ord, head, tail...);
-}
-
-bool in_rect(int i, int j, int h, int w) {
-    return 0 <= i and i < h and 0 <= j and j < w;
-}
-
-template<class T>
-constexpr vector<T> pow_table(int n, T base) {
-    vector<T> res(n, 1);
-    rep(i, n - 1) res[i + 1] = res[i] * base;
-    return res;
-}
-
-template<class T, class S>
-vector<T> cumsum(const vector<S> &v, bool shift_one = true) {
-    int n = v.size();
-    vector<T> res;
-    if (shift_one) {
-        res.resize(n + 1);
-        rep(i, n) res[i + 1] = res[i] + v[i];
-    } else {
-        res.resize(n);
-        if (n) {
-            res[0] = v[0];
-            rep(i, 1, n) res[i] = res[i - 1] + v[i];
-        }
-    }
-    return res;
-}
-
-vvi graph(int n, int m, bool directed = false, int origin = 1) {
-    vvi G(n);
-    rep(_, m) {
-        INT(u, v);
-        u -= origin, v -= origin;
-        G[u].pb(v);
-        if (!directed) G[v].pb(u);
-    }
-    return G;
-}
-
-template<class T>
-vector<vector<pair<int, T>>> weighted_graph(int n, int m, bool directed = false, int origin = 1) {
-    vector<vector<pair<int, T>>> G(n);
-    rep(_, m) {
-        int u, v;
-        T w;
-        scan(u, v, w);
-        u -= origin, v -= origin;
-        G[u].eb(v, w);
-        if (!directed) G[v].eb(u, w);
-    }
-    return G;
-}
-
-void solve() {
-    INT(n, m);
-    vi c(n);
-    scan(c);
-    vvi G = graph(n, m);
-    vvi dist(n, vi(n, inf));
-    dist[0][n - 1] = 0;
-    queue<P> q;
-    q.emplace(0, n - 1);
-    while (not q.empty()) {
-        auto [u1, u2] = q.front();
-        q.pop();
-        for (int v1: G[u1]) {
-            for (int v2: G[u2]) {
-                if (c[v1] == c[v2]) continue;
-                if (chmin(dist[v1][v2], dist[u1][u2] + 1)) q.emplace(v1, v2);
-            }
-        }
-    }
-    int ans = dist[n - 1][0];
-    print(ans == inf ? -1 : ans);
-}
-
-int main() {
-    int t;
-    cin >> t;
-    rep(i, t) solve();
+int main(){
+    print(lcm(8,5));
 }
